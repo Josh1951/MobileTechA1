1dd5a123eecb67e4ddd088e7ecb967653734e061
Josh <har17001504@student.citybathcoll.ac.uk>
2019-05-08 20:16:46 +0100

    Configure groups fragment and layout.

--- app/src/main/java/fragments/GroupsFragment.java
+++ app/src/main/java/fragments/GroupsFragment.java
@@ -1,19 +1,53 @@
 package fragments;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.joshuaharveya1.MyAdapter;
 import com.example.joshuaharveya1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class GroupsFragment extends Fragment {
+
+    private RecyclerView rv;
+    private static String lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    private static String[] headlines={"Chess Group","Sports Club","LGBTQA","Film Club","Bath Politics"};
+    private static String[] descriptions={lipsum, lipsum, lipsum, lipsum, lipsum};
+    private static int image = R.drawable.ic_groups;
+
+    public static GroupsFragment newInstance()
+    {
+        return new GroupsFragment();
+    }
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView=inflater.inflate(R.layout.fragment_groups,null);
+
+        //REFERENCE
+        rv= (RecyclerView) rootView.findViewById(R.id.groups_RV);
+
+        //LAYOUT MANAGER
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //ADAPTER
+        rv.setAdapter(new MyAdapter(getActivity(),headlines, descriptions, image));
+
+        return rootView;
     }
+
 }
