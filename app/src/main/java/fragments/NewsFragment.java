594207ffb51d2510886d00ca3c533d518ad9cf7d
Josh <har17001504@student.citybathcoll.ac.uk>
2019-05-08 14:27:20 +0100

    Convert news fragment to constraint layout, re size recyclerview. add dummy text.

--- app/src/main/java/fragments/NewsFragment.java
+++ app/src/main/java/fragments/NewsFragment.java
@@ -16,8 +16,9 @@ import com.example.joshuaharveya1.R;
 public class NewsFragment extends Fragment {
 
     private RecyclerView rv;
+    private static String lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     private static String[] headlines={"Headline 1","Headline 2","Headline 3","Headline 4","Headline 5"};
-    private static String[] descriptions={"Headline x","xxxxxx","Hblurgh","something","something"};
+    private static String[] descriptions={lipsum, lipsum, lipsum, lipsum, lipsum};
 
     public static NewsFragment newInstance()
     {
